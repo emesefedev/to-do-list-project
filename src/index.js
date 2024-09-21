9614c87b4d45a764c94b1f0c43dd41397f3ebb76
emesefedev <emesefedev@gmail.com>
2024-09-21 20:38:19 +0200

    project object

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import "./style.css"
 import ToDoItem from "./to-do-item.js"
+import Project from "./project.js"
 
 console.log("Hello emesefedev")
+
+const defaultProject = new Project({name: "Default Project"})
 
 const toDo = new ToDoItem({
     title: "New ToDo", 
     dueDate: new Date(2024, 11, 1), 
-    project: "default"})
+    project: defaultProject})
 
 console.log(toDo)
